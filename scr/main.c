b4b9557bd072d44cec48e199ec5ef3defe4a206f
Wanderson Guedes <wg21249@gmail.com>
2025-08-20 16:29:58 -0300

    executavel da engine funcional

--- scr/main.c
+++ scr/main.c
@@ -1,21 +1,19 @@
 #include "raylib.h"
 
 int main(void) {
-    // Inicializa a janela
+
     InitWindow(800, 600, "Meu primeiro jogo em C");
     SetTargetFPS(60);
 
-    // Player básico
-    Rectangle player = { 400, 500, 50, 50 }; // x, y, width, height
+    Rectangle player = { 400, 500, 50, 50 };
     int speed = 5;
 
     while (!WindowShouldClose()) {
-        // Input
+
         if (IsKeyDown(KEY_RIGHT)) player.x += speed;
         if (IsKeyDown(KEY_LEFT))  player.x -= speed;
 
-        // Render
-        BeginDrawing();
+            BeginDrawing();
             ClearBackground(RAYWHITE);
             DrawRectangleRec(player, BLUE);
         EndDrawing();

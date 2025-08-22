2ded917b3e61eb2becf3487d9b306e1993cd5949
Wanderson Guedes <wg21249@gmail.com>
2025-08-21 23:10:35 -0300

    gameplay basica funcional

--- scr/main.c
+++ scr/main.c
@@ -1,21 +1,117 @@
 #include "raylib.h"
+#include "../include/player.h"
+
+
+#define MAX_BULLETS 100
+#define MAX_ENEMIES 50
 
 int main(void) {
 
-    InitWindow(800, 600, "Meu primeiro jogo em C");
+    InitWindow(800, 600, "Staquito Adventure");
     SetTargetFPS(60);
 
-    Rectangle player = { 400, 500, 50, 50 };
-    int speed = 5;
+ 
+    Player player;
+    InitPlayer(&player);
+    
+ 
+    Vector2 bullets[MAX_BULLETS];
+    int bulletCount = 0;
+    float bulletSpeed = 7.0f;
+    
+
+    Rectangle enemies[MAX_ENEMIES];
+    int enemyCount = 0;
+    float enemySpawnTimer = 0;
 
     while (!WindowShouldClose()) {
+     
+        UpdatePlayer(&player);
+        
+        Shoot(&player, bullets, &bulletCount, MAX_BULLETS);
+        
+        for (int i = 0; i < bulletCount; i++) {
+            bullets[i].y -= bulletSpeed;
+            
+            if (bullets[i].y < 0) {
+                for (int j = i; j < bulletCount - 1; j++) {
+                    bullets[j] = bullets[j + 1];
+                }
+                bulletCount--;
+                i--;
+            }
+        }
+        
+        enemySpawnTimer += GetFrameTime();
+        if (enemySpawnTimer >= 2.0f && enemyCount < MAX_ENEMIES) {
+            enemies[enemyCount] = (Rectangle){
+                GetRandomValue(0, 800 - 30),
+                -30,
+                30,
+                30
+            };
+            enemyCount++;
+            enemySpawnTimer = 0;
+        }
+        
+   
+        for (int i = 0; i < enemyCount; i++) {
+            enemies[i].y += 2.0f;
+            
+    
+            if (enemies[i].y > 600) {
+                for (int j = i; j < enemyCount - 1; j++) {
+                    enemies[j] = enemies[j + 1];
+                }
+                enemyCount--;
+                i--;
+            }
+        }
+        
+     
+        for (int i = 0; i < bulletCount; i++) {
+            for (int j = 0; j < enemyCount; j++) {
+                if (CheckCollisionPointRec(bullets[i], enemies[j])) {
+                  
+                    for (int k = i; k < bulletCount - 1; k++) {
+                        bullets[k] = bullets[k + 1];
+                    }
+                    bulletCount--;
+                    i--;
+                    
+                    for (int k = j; k < enemyCount - 1; k++) {
+                        enemies[k] = enemies[k + 1];
+                    }
+                    enemyCount--;
+                    j--;
+                    
+                    player.score += 10;
+                    break;
+                }
+            }
+        }
 
-        if (IsKeyDown(KEY_RIGHT)) player.x += speed;
-        if (IsKeyDown(KEY_LEFT))  player.x -= speed;
-
-            BeginDrawing();
+   
+        BeginDrawing();
             ClearBackground(RAYWHITE);
-            DrawRectangleRec(player, BLUE);
+            
+            // Desenha o player
+            DrawPlayer(&player);
+            
+            // Desenha projéteis
+            for (int i = 0; i < bulletCount; i++) {
+                DrawCircleV(bullets[i], 3, RED);
+            }
+            
+            // Desenha inimigos
+            for (int i = 0; i < enemyCount; i++) {
+                DrawRectangleRec(enemies[i], DARKGRAY);
+            }
+            
+            // Desenha UI
+            DrawText(TextFormat("Score: %d", player.score), 10, 10, 20, BLACK);
+            DrawText(TextFormat("Health: %d", player.health), 10, 40, 20, BLACK);
+            
         EndDrawing();
     }
 
---
+++ scr/player.c
@@ -0,0 +1,99 @@
+#include "../include/player.h"
+#include "raylib.h"
+
+
+void InitPlayer(Player *player)
+{
+    player->position = (Vector2){400, 500};
+    player->size = (Vector2){50, 50};
+    player->color = BLUE;
+    player->speed = 10.0f;
+    player->dashSpeed = 15.0f;
+    player->dashCooldown = 2.0f;
+    player->dashDuration = 1.0f;
+    player->isDashing = false;
+    player->health = 100;
+    player->score = 0;
+    player->weaponType = 0;
+}
+
+void UpdatePlayer(Player *player) {
+    if (player->dashCooldown > 0) {
+        player->dashCooldown -= GetFrameTime();
+    }
+    
+    if (player->isDashing) {
+        player->dashDuration -= GetFrameTime();
+        if (player->dashDuration <= 0) {
+            player->isDashing = false;
+        }
+        return;
+    }
+    
+    float currentSpeed = player->speed;
+    if (IsKeyDown(KEY_RIGHT)) player->position.x += currentSpeed;
+    if (IsKeyDown(KEY_LEFT)) player->position.x -= currentSpeed;
+    
+
+    if (player->position.x < 0) player->position.x = 0;
+    if (player->position.x > 800 - player->size.x) player->position.x = 800 - player->size.x;
+    
+    if (IsKeyPressed(KEY_LEFT_SHIFT) && player->dashCooldown <= 0) {
+        Dash(player);
+    }
+}
+
+
+void Dash(Player *player) {
+    player->isDashing = true;
+    player->dashDuration = 0.2f;
+    player->dashCooldown = 1.0f;
+    
+
+    float dashDirection = 0;
+    if (IsKeyDown(KEY_RIGHT)) dashDirection = 1;
+    if (IsKeyDown(KEY_LEFT)) dashDirection = -1;
+    
+    if (dashDirection == 0) dashDirection = 1;
+    
+    player->position.x += dashDirection * player->dashSpeed * 5;
+}
+
+void Shoot(Player *player, Vector2 *bullets, int *bulletCount, int maxBullets) {
+    if (IsKeyPressed(KEY_SPACE)) {
+        switch (player->weaponType) {
+            case 0:
+                if (*bulletCount < maxBullets - 1) {
+                    bullets[*bulletCount] = (Vector2){player->position.x + player->size.x/2, player->position.y};
+                    (*bulletCount)++;
+                }
+                break;
+                
+            case 1:
+                if (*bulletCount < maxBullets - 2) {
+                    bullets[*bulletCount] = (Vector2){player->position.x + player->size.x/4, player->position.y};
+                    bullets[*bulletCount + 1] = (Vector2){player->position.x + 3*player->size.x/4, player->position.y};
+                    *bulletCount += 2;
+                }
+                break;
+                
+        }
+    }
+}
+
+
+void DrawPlayer(Player *player) {
+ 
+    Color drawColor = player->color;
+    if (player->isDashing) {
+        drawColor = (Color){drawColor.r, drawColor.g, drawColor.b, 180};
+    }
+    
+    DrawRectangleV(player->position, player->size, drawColor);
+    
+    if (player->dashCooldown > 0) {
+        float cooldownPercent = 1.0f - (player->dashCooldown / 1.0f);
+        DrawRectangle((int)player->position.x, (int)player->position.y - 10, 
+                     (int)(player->size.x * cooldownPercent), 5, GREEN);
+    }
+}
